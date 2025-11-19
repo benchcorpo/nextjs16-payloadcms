34541fc0bc5bd5ae6b273279b25fa6cbdc32384d
pmall <pierre.mallinjoud@gmail.com>
2025-11-19 17:22:01 +0100

    chore: Configure template to exclude migrations Added migrations/.gitignore to ignore migration files.
    Rationale:
    This template serves as a base for client projects.
    Each client clone will generate its own migrations based on the globals/collections they need.
    Migration files will be committed in client repositories, not in this template.

--- migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251119_151420 from './20251119_151420';
-
-export const migrations = [
-  {
-    up: migration_20251119_151420.up,
-    down: migration_20251119_151420.down,
-    name: '20251119_151420'
-  },
-];
