d61e1947d2aa6f40f9c93f52d74812bf54dab285
pmall <pierre.mallinjoud@gmail.com>
2025-12-05 18:24:00 +0100

    remove useless file

--- src/features/index.ts
+++
@@ -1,9 +0,0 @@
-import { CollectionConfig, GlobalConfig, Payload } from "payload";
-
-type Seed = (payload: Payload) => Promise<void>;
-
-export type Feature = {
-  globals: GlobalConfig[];
-  collections: CollectionConfig[];
-  seeds: Seed[];
-};
